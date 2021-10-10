b9734ab1f492eb90f77f6d844bc972085d7e9915
speak <brendanshen@hotmail.com>
2021-10-10 09:22:10 -0700

    added listbranch to list-branches.tsx

--- source/boards/list-branches.tsx
+++ source/boards/list-branches.tsx
@@ -1,3 +1,33 @@
-/**
- * Make a components which takes as input a command, gets the output of that command and displays it in a listt stat
- */
+import React, {useEffect, useState} from "react";
+import {Text} from "ink";
+import { exec } from 'child_process';
+
+const ListBranch = ({command}: {command: string}) => {
+	const [color, setColor] = useState('green');
+	const [output, setOutput] = useState<string[]>([]);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			exec(command, (error, stdout, stderr) => {
+				if (error) {
+					setOutput(error.message.split('\n'))
+					setColor('red')
+					return;
+				}
+				if (stderr) {
+					setOutput(stderr.split('\n'))
+					setColor('red')
+					return;
+				}
+				setOutput(stdout.split('\n'))
+				setColor('green')
+			})
+		}, 100);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
+	return output.map(outputLine => <Text color={color}>{outputLine} </Text>)
+}
+export default ListBranch
