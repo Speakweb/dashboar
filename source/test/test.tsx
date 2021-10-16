4d18a59689a06cb18a61029bdf5c3694e693d3ab
Marvin <marvin@marvinirwin.com>
2021-10-16 17:55:35 -0400

    added tests

--- source/test/test.tsx
+++ source/test/test.tsx
@@ -1,22 +1,32 @@
 import test from 'ava';
 import response from './fixtures/response'
 import getRepositoryNames from '../lib/getRepositoryNames'
+import React from 'react';
+import {render} from 'ink-testing-library';
+import RepositoryList from "../RepositoryList";
+
+const expectedResult = [
+	"Drupal TASWeb Authentication Module",
+	"Internal Events",
+	"Brand Stores CMS",
+	"activeCollab",
+	"Montreal iPad",
+	"Web Project Plans",
+	"Friday Sessions",
+	"Arcteryx.com Behat Test Suite",
+	"WebScripts (pre-2011)",
+	"Sales Meeting (pre-2013)"
+];
 
 
 test("getRepositoryNames", t => {
-	const expectedResult = [
-		"Drupal TASWeb Authentication Module",
-		"Internal Events",
-		"Brand Stores CMS",
-		"activeCollab",
-		"Montreal iPad",
-		"Web Project Plans",
-		"Friday Sessions",
-		"Arcteryx.com Behat Test Suite",
-		"WebScripts (pre-2011)",
-		"Sales Meeting (pre-2013)"
-	];
 	const actualResponse = getRepositoryNames(response as BitBucketResponse.RootObject);
 	t.deepEqual(actualResponse, expectedResult);
 
 })
+
+
+test('greet user with a name', t => {
+	const {lastFrame} = render(<RepositoryList repositories={response.data.values}/>);
+	t.is(lastFrame(), expectedResult.join('\n'));
+});
