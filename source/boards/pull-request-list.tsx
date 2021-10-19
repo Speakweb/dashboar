105f7318b4dc588bc575b44be229913235123259
Anton-Pan <anton.v.panfilenok@gmail.com>
2021-10-18 17:59:59 -0700

    pull request list stuff

---
+++ source/boards/pull-request-list.tsx
@@ -0,0 +1,19 @@
+import {Text} from "ink";
+import React from "react";
+
+export interface PullRequest {
+	title: string
+}
+
+export const PullRequestList: ({pullRequests}: { pullRequests: PullRequest[] }) => React.ReactElement[] = (
+	{
+		pullRequests
+	}
+) => {
+	let PullReqList: React.ReactElement[] = []
+	pullRequests.forEach(PullRequest => {
+			PullReqList.push(<Text key={PullRequest.title}>{PullRequest.title}</Text>)
+		}
+	)
+	return PullReqList
+}
