ff1db2f47c388c31ff1288beecf8cfc351f8050e
Marvin <marvin@marvinirwin.com>
2021-10-18 20:25:28 -0400

    added test success condition

--- source/test/test.tsx
+++ source/test/test.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import {render} from 'ink-testing-library';
 import RepositoryList from "../RepositoryList";
 import PullRequestsResponse from "./fixtures/pull-requests-response";
+import BitBucketRepository = BitBucketResponse.BitBucketRepository;
 
 const expectedResult = [
 	"Drupal TASWeb Authentication Module",
@@ -21,14 +22,14 @@ const expectedResult = [
 
 
 test("getRepositoryNames", t => {
-	const actualResponse = getRepositoryNames(response as BitBucketResponse.RootObject);
+	const actualResponse = getRepositoryNames(RepositoriesResponse as BitBucketResponse.RootObject);
 	t.deepEqual(actualResponse, expectedResult);
 
 })
 
 
 test('greet user with a name', t => {
-	const {lastFrame} = render(<RepositoryList repositories={RepositoriesResponse.data}/>);
+	const {lastFrame} = render(<RepositoryList repositories={RepositoriesResponse.data.values}/>);
 	t.is(lastFrame(), expectedResult.join('\n'));
 });
 
@@ -37,6 +38,6 @@ function PulLRequestList() {
 }
 
 test("rendering of pull requests", t => {
-	const {lastFrame} = render(<PulLRequestList pullRequests={PullRequestsResponse.data}/>);
-	t.is(lastFrame(), expectedResult.join('\n'));
+	const {lastFrame} = render(<PulLRequestList pullRequests={PullRequestsResponse.data.values}/>);
+	t.is(lastFrame(), 'Release/oct 14 2021');
 })
