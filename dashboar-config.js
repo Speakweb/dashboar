617c94e47a1635516920431db09f06fccfcd3905
Marvin <marvin@marvinirwin.com>
2021-10-18 22:42:58 -0400

    listed the pull requests for one repo

--- dashboar-config.js
+++ dashboar-config.js
@@ -4,5 +4,15 @@ module.exports = {
 	`cd /Users/marvinirwin/WebstormProjects/forgerock/arcteryx-checkout && echo "checkout $(git branch --show-current)"`,
 	`cd /Users/marvinirwin/WebstormProjects/forgerock/catalog-pages && echo "catalog-pages $(git branch --show-current)"`,
 	`cd /Users/marvinirwin/WebstormProjects/arcteryx/arcteryx-js-helpers && echo "js-helpers $(git branch --show-current)"`,
-  ]
+  ],
+	pullRequestConfigs: [
+		{
+			workspace: "arcteryx",
+			repo: 'arcteryx-checkout'
+		},
+		{
+			workspace: "arcteryx",
+			repo: 'arcteryx-js-helpers'
+		}
+	]
 }
--- source/cli.tsx
+++ source/cli.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import {render} from 'ink';
 import App, {Config} from './ui';
 import {join} from "path";
+import {fetchPullRequests} from "./fetch-bitbucket";
 
-const config: Config = require(join(process.cwd(), './dashboar-config'));
+const loadedConfiguration: Config = require(join(process.cwd(), './dashboar-config'));
 
-render(<App config={config}/>);
+render(<App
+	config={loadedConfiguration}
+	pullRequestFetchFunction={config => () => fetchPullRequests(config)}
+/>);
