98e82838cee5fd2823f6accd41e46de892d122fb
Marvin <marvin@marvinirwin.com>
2021-10-18 21:38:07 -0400

    results

---
+++ source/lib/paginate.ts
@@ -0,0 +1,16 @@
+export const consumePaginated = async <T>({
+										fetchFunction,
+										interval
+}: {fetchFunction: (n: number) => Promise<T[]>, interval: number}): Promise<T[]> => {
+	let n = 0;
+	const allResults = [];
+	let fetchedResults: T[] | undefined;
+	while(fetchedResults = await fetchFunction(n)) {
+		if (!fetchedResults?.length) {
+			break;
+		}
+		allResults.push(...fetchedResults)
+		n += interval;
+	}
+	return fetchedResults;
+}
