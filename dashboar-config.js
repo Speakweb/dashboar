3636030c0aa9cbcedd4b2a419658aedbb65380fe
Anton-Pan <anton.v.panfilenok@gmail.com>
2021-10-21 21:00:14 -0700

    added box around stuff

--- dashboar-config.js
+++ dashboar-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "commands": [
+  commands: [
 	`cd /Users/marvinirwin/WebstormProjects/arcteryx/account-pages && echo "account-pages $(git branch --show-current)"`,
 	`cd /Users/marvinirwin/WebstormProjects/forgerock/arcteryx-checkout && echo "checkout $(git branch --show-current)"`,
 	`cd /Users/marvinirwin/WebstormProjects/forgerock/catalog-pages && echo "catalog-pages $(git branch --show-current)"`,
---
+++ dashboar.unix.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	commands: ["date"],
+	pullRequestConfigs:[
+		{
+			workspace: "test-dashboar",
+			repo: "test-dashboar-repo"
+		}
+		{
+			workspace: "test-dashboar",
+			repo: "test-dashboar-repo-2"
+		}
+	],
+}
