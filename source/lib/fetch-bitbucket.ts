9f0fd0fdf4b2b069f72a035abf5994184463198b
Marvin <marvin@marvinirwin.com>
2021-10-21 20:30:11 -0400

    added fetch repos function

--- source/lib/fetch-bitbucket.ts
+++ source/lib/fetch-bitbucket.ts
@@ -8,10 +8,14 @@ const clientOptions = {
 	},
 }
 
-
 const bitbucket = new Bitbucket(clientOptions);
 
 export const fetchPullRequests = ({repo, workspace}: {repo: string, workspace: string}): Promise<PullRequest[]> => {
 	return bitbucket.pullrequests.list({repo_slug: repo, workspace})
 		.then((result: any)  => result.data.values)
+}
+
+export const fetchRepositories = ({workspace}:{workspace: string}) => {
+	// TODO, make this work with pages.  For some reason the page param is a string?  Yet pageLen is a number?
+	return bitbucket.repositories.list({pagelen: 100, workspace});
 }
