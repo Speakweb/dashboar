e5e34c003921badf8788fca067ded81087f72c7b
Marvin <marvin@marvinirwin.com>
2021-10-24 19:25:35 -0400

    added scaffold for pull-requests.tsx error handling

--- source/components/pull-requests.tsx
+++ source/components/pull-requests.tsx
@@ -1,4 +1,5 @@
 import {PullRequest, PullRequestList} from "./pull-request-list";
+import {Text} from "ink";
 import React, {useEffect, useState} from "react";
 
 const ONE_MINUTE = 60000;
@@ -7,11 +8,26 @@ export function PullRequests({fetchFunction}: {
     fetchFunction: () => Promise<PullRequest[]>
 }) {
     const [pullRequests, setPulLRequests] = useState<PullRequest[]>([]);
+    const [fetchingError, setFetchingError] = useState<string>('');
+
     useEffect(() => {
-		fetchFunction().then(values => setPulLRequests(values))
+
+		fetchFunction()
+			.then(values => setPulLRequests(values))
+			.then(() => setFetchingError(''))
+		// Add your .catch here
+
 		setInterval(() => {
-			fetchFunction().then(values => setPulLRequests(values))
+			fetchFunction()
+				.then(values => setPulLRequests(values))
+				.then(() => setFetchingError(''))
+			// Add your .catch here
+
         }, ONE_MINUTE)
-    }, [])
-    return <PullRequestList pullRequests={pullRequests}/>;
+    }, []);
+
+	if (fetchingError) {
+		return <Text>{fetchingError}</Text>
+	}
+    return  <PullRequestList pullRequests={pullRequests}/>
 }
