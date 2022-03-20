3a1086334def52e48c2cc37aced5285a5b1af120
shinjam3 <james-shin-1998@hotmail.com>
2022-03-20 18:52:38 -0400

    created a test script that stores credentials

---
+++ dashboar.config.epub-finder.js
@@ -0,0 +1,17 @@
+module.exports = {
+    sshTunnels: [
+      // Tell dashboar that it’s needs to be use whatever widget is used for ssh tunnels
+      {
+        storeKey: "keyCloakSshTunnel", // Tell dashboar that it needs this value to be present in the encrypted store.  If it isn’t, then query it
+      },
+    ],
+    commands: [
+      {
+        storeKeys: {
+          databaseHost: { type: "string" }, // StoreKeys can have type hints if they want
+        },
+        command: ({ databaseHost }) => `ping ${databaseHost}`, // commands can be function which receive their store args as parameters
+      },
+    ],
+  };
+  
--- source/lib/config.ts
+++ source/lib/config.ts
@@ -1,1 +1,12 @@
 export type Config = { commands: string[], pullRequestConfigs: { workspace: string, repo: string }[] };
+
+type StoreSshTunnel = { storeKey: string };
+type StoreCommand = {
+  storeKeys: {
+    databaseHost: {
+      type: string 
+    }
+  },
+  command: any
+};
+export type StoreConfig = { sshTunnels: [StoreSshTunnel], commands: [StoreCommand]}
