b6901f7ff6d5220493af02f5745763d8cca36603
shinjam3 <james-shin-1998@hotmail.com>
2022-03-29 23:01:22 -0400

    edited get-store-credentials and its test file

--- source/lib/get-store-credentials.test.ts
+++ source/lib/get-store-credentials.test.ts
@@ -1,25 +1,72 @@
 import test from "ava";
+const sinon = require("sinon");
 import { getStoreCredentials } from "./get-store-credentials";
-import fs from 'fs';
-import { Store } from "./config";
+import fs from "fs";
+import readline from "readline";
+import { join } from "path";
+import { StoreConfig, Store } from "./config";
+
+const loadedConfiguration: StoreConfig = require(join(
+  process.cwd(),
+  "./dashboar.config.epub-finder.js"
+));
+const storeFilePath = "dashboar-store";
+let readlineStub: any;
+
+test.afterEach(() => {
+  if (fs.existsSync(storeFilePath)) fs.unlinkSync(storeFilePath);
+  if (readlineStub) readlineStub.restore();
+});
+
+test.serial("Prompts the user for values if the store is empty", async (t) => {
+  let results: any = [];
+
+  const readlineInterfaceStub = {
+    question: sinon.stub().callsFake((query: string, callback: any) => {
+      results.push(query);
+      return callback("pw");
+    }),
+    close: sinon.stub(),
+  };
+
+  readlineStub = sinon
+    .stub(readline, "createInterface")
+    .returns(readlineInterfaceStub);
+
+  await getStoreCredentials();
+  t.true(results[0] === "What is the password for the store? ");
+  t.true(results[1] === loadedConfiguration.prompts.databaseConnectionString);
+  t.true(results[2] === loadedConfiguration.prompts.sshString);
+});
+
+// todo: test("Does not prompt the user for values which are already present", async (t) => {});
+
+test.serial("Writes to a store file", async (t) => {
+  const readlineInterfaceStub = {
+    question: sinon.stub().callsFake((query: string, callback: any) => {
+      if (query.indexOf("database") >= 0) return callback("test_db");
+      else if (query.indexOf("SSH") >= 0) return callback("test_ssh");
+      else return callback("pw");
+    }),
+    close: sinon.stub(),
+  };
+
+  readlineStub = sinon
+    .stub(readline, "createInterface")
+    .returns(readlineInterfaceStub);
+
+  // testing to see if dashboar-store exists
+  await getStoreCredentials();
+  t.true(fs.existsSync(storeFilePath));
+
+  // testing to see if the store values are properly stored
+  const buffer = fs.readFileSync(storeFilePath);
+  const data: Store = JSON.parse(buffer.toString());
+  t.is(data.databaseConnectionString, "test_db");
+  t.is(data.sshString, "test_ssh");
+});
 
 /* todo:
-//Writing to store file and reading it
-test(“Writes to a store file”, () => {})
 test(“Reads from a store file with the correct password”, () => {})
 test(“Cannot read from the store file with an incorrect password”, () => {})
 */
-
-const storeFilePath = 'dashboar-store';
-
-// Prompting the user for store inputs
-test("Prompts to user for values if the store is empty", async (t) => {
-    await getStoreCredentials();
-    t.truthy(fs.existsSync(storeFilePath));
-
-    const buffer = fs.readFileSync(storeFilePath);
-    const data: Store = JSON.parse(buffer.toString());
-    t.is(data.databaseConnectionString, 'test_db'); // assuming user input is 'test_db'
-    t.is(data.sshString, 'test_ssh'); // assuming user input is 'test_ssh'
-});
-//test("Does not prompt the user for values which are already present", async (t) => {});
--- source/lib/get-store-credentials.ts
+++ source/lib/get-store-credentials.ts
@@ -1,4 +1,4 @@
-import readline from 'readline';
+import readline from "readline";
 import { promises as fs, existsSync } from "fs";
 import { join } from "path";
 import { StoreConfig, Store, StoreKey } from "./config";
@@ -10,17 +10,17 @@ const loadedConfiguration: StoreConfig = require(join(
 
 const storeFilePath = "dashboar-store";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 export const getStoreCredentials = async () => {
   if (existsSync(storeFilePath)) {
     //const buffer = fs.readFileSync(storeFilePath);
     //const data: Store = JSON.parse(buffer.toString());
     // todo: check if values from config file are present in store file
   } else {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
     console.log("\nCould not find input file, creating one: ./dashboar-store");
 
     const values: Store = {
@@ -28,22 +28,37 @@ export const getStoreCredentials = async () => {
       sshString: "",
     };
 
+    // Prompts the user for values based off the questions in the config file
     const prompt = async (key: StoreKey) => {
       return new Promise<void>((resolve, reject) => {
         rl.question(loadedConfiguration.prompts[key], async (dbString: string) => {
-            values[key] = dbString;
-            try {
-              await fs.writeFile(storeFilePath, JSON.stringify(values));
-              console.log(`Wrote ${key} to store`);
-              resolve();
-            } catch {
-              reject();
-            }
+          values[key] = dbString;
+          try {
+            await fs.writeFile(storeFilePath, JSON.stringify(values));
+            console.log(`Wrote ${key} to store`);
+            resolve();
+          } catch {
+            reject();
           }
-        );
+        });
       });
     };
 
+    const createStore = async () => {
+      return new Promise<void>((resolve, reject) => {
+        rl.question("What is the password for the store? ", async (pw: string) => {
+          if (!pw.length) reject();
+          else {
+            console.log("Created dashboar-store");
+            // todo: encrypt file with pw
+
+            resolve();
+          }
+        });
+      });
+    };
+
+    await createStore();
     let key: StoreKey | any;
     for (key in loadedConfiguration.prompts) {
       await prompt(key);
