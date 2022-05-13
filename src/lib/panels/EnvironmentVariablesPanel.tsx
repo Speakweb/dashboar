9310c236635f44d9c0bd094f948d82a31702a474
shinjam3 <james-shin-1998@hotmail.com>
2022-05-12 23:52:01 -0400

    environment variables checker panel

---
+++ src/lib/panels/EnvironmentVariablesPanel.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box } from "ink";
+import { EnvironmentVariablesList } from "../../components/EnvironmentVariablesList";
+
+export class EnvironmentVariablesPanel {
+  protected envVarList: [string];
+
+  constructor(envVarList: [string]) {
+    this.envVarList = envVarList;
+  }
+
+  Component({ key }: { key?: string | number }) {
+    return (
+      <Box borderStyle="single" flexDirection="column" key={key}>
+        <EnvironmentVariablesList envVarList={this.envVarList} />
+      </Box>
+    );
+  }
+}
