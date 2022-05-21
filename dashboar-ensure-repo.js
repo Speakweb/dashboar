39b675004f62780021bf241cadfb1656dd909460
allbombson <24781964+allbombson@users.noreply.github.com>
2022-05-21 09:51:37 -0700

    Quick fix

--- dashboar-ensure-repo.js
+++ dashboar-ensure-repo.js
@@ -2,11 +2,11 @@ module.exports = {
 	repositoryConfigs: [
 		{
 			configKey: "something something",
-			urls: "https://github.com/marvinirwin/epub-finder"
+			url: "https://github.com/marvinirwin/epub-finder"
 		},
 		{
 			configKey: "something something2",
-			urls: "https://github.com/marvinirwin/graph-paper"
+			url: "https://github.com/marvinirwin/graph-paper"
 		}
 	]
 }
